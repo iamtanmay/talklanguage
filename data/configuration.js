ad586c0bf52840f1931eacba295caa4c9cbefa83
iamtanmay <iamtanmay@gmail.com>
2013-04-26 10:24:13 +0200

    - gh pages up

---
+++ data/configuration.js
@@ -0,0 +1,8 @@
+﻿var configuration = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,e);}; 
+var b="showPageNotes",c=true,d="showPageNoteNames",e=false,f="logoImagePath",g="data\/logo.jpg",h="logoImageCaption",i="This is a sample prototype of a healthy foods site created in Axure RP. In this sample, you will see some elements frequently found in branded websites.",j="loadFeedbackPlugin";
+return _creator();
+})()
