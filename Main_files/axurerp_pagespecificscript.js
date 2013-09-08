ed8b72f167565960e8cc00cfd68719a1d054888c
iamtanmay <iamtanmay@gmail.com>
2013-09-08 23:16:15 +0200

    - Final cut

--- Main_files/axurerp_pagespecificscript.js
+++ Main_files/axurerp_pagespecificscript.js
@@ -1,4 +1,4 @@
-for(var i = 0; i < 24; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+for(var i = 0; i < 37; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
@@ -13,25 +13,33 @@ if (true) {
 }
 
 }
-gv_vAlignTable['u3'] = 'center';
-u21.style.cursor = 'pointer';
-$axure.eventManager.click('u21', function(e) {
+
+u27.style.cursor = 'pointer';
+$axure.eventManager.click('u27', function(e) {
 
 if (true) {
 
-	SetPanelState('u14', 'pd1u14','none','',500,'none','',500);
+	NewTab('https://sites.google.com/site/iowagapps/google/setting-up-google-hangouts', "");
 
 }
 });
-gv_vAlignTable['u16'] = 'center';document.getElementById('u4_img').tabIndex = 0;
-HookHover('u4', false);
-
-u4.style.cursor = 'pointer';
-$axure.eventManager.click('u4', function(e) {
+gv_vAlignTable['u16'] = 'center';gv_vAlignTable['u3'] = 'center';
+u28.style.cursor = 'pointer';
+$axure.eventManager.click('u28', function(e) {
 
 if (true) {
 
-	SetPanelState('u14', 'pd1u14','none','',500,'none','',500);
+	SetPanelState('u14', 'pd2u14','none','',500,'none','',500);
+
+}
+});
+
+u29.style.cursor = 'pointer';
+$axure.eventManager.click('u29', function(e) {
+
+if (true) {
+
+	NewTab('https://support.google.com/plus/answer/1355579?hl=en', "");
 
 }
 });
@@ -43,35 +51,11 @@ $axure.eventManager.click('u8', function(e) {
 
 if (true) {
 
-	NewTab('http://www.facebook.com/pages/Kanno-Yoko/10505488303', "");
+	SetPanelState('u14', 'pd2u14','none','',500,'none','',500);
 
 }
 });
-document.getElementById('u10_img').tabIndex = 0;
-HookHover('u10', false);
-
-u10.style.cursor = 'pointer';
-$axure.eventManager.click('u10', function(e) {
-
-if (true) {
-
-	NewTab('http://www.facebook.com/pages/Kanno-Yoko/10505488303', "");
-
-}
-});
-document.getElementById('u22_img').tabIndex = 0;
-HookHover('u22', false);
-
-u22.style.cursor = 'pointer';
-$axure.eventManager.click('u22', function(e) {
-
-if (true) {
-
-	NewTab('http://www.facebook.com/pages/Kanno-Yoko/10505488303', "");
-
-}
-});
-gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u20'] = 'center';document.getElementById('u6_img').tabIndex = 0;
+document.getElementById('u6_img').tabIndex = 0;
 HookHover('u6', false);
 
 u6.style.cursor = 'pointer';
@@ -79,7 +63,51 @@ $axure.eventManager.click('u6', function(e) {
 
 if (true) {
 
-	SetPanelState('u14', 'pd2u14','none','',500,'none','',500);
+	SetPanelState('u14', 'pd4u14','none','',500,'none','',500);
+
+}
+});
+document.getElementById('u35_img').tabIndex = 0;
+HookHover('u35', false);
+
+u35.style.cursor = 'pointer';
+$axure.eventManager.click('u35', function(e) {
+
+if (true) {
+
+	SetPanelState('u14', 'pd3u14','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u13'] = 'center';document.getElementById('u4_img').tabIndex = 0;
+HookHover('u4', false);
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	SetPanelState('u14', 'pd1u14','none','',500,'none','',500);
+
+}
+});
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u7'] = 'center';
+u23.style.cursor = 'pointer';
+$axure.eventManager.click('u23', function(e) {
+
+if (true) {
+
+	NewTab('http://snipmp3.com/', "");
+
+}
+});
+
+u24.style.cursor = 'pointer';
+$axure.eventManager.click('u24', function(e) {
+
+if (true) {
+
+	NewTab('http://keepvid.com/', "");
 
 }
 });
@@ -95,4 +123,45 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u7'] = 'center';
+gv_vAlignTable['u18'] = 'center';document.getElementById('u10_img').tabIndex = 0;
+HookHover('u10', false);
+
+u10.style.cursor = 'pointer';
+$axure.eventManager.click('u10', function(e) {
+
+if (true) {
+
+	NewTab('http://hinditeacher.boards.net/index.cgi', "");
+
+}
+});
+gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u5'] = 'center';
+u22.style.cursor = 'pointer';
+$axure.eventManager.click('u22', function(e) {
+
+if (true) {
+
+	NewTab('http://keepvid.com/', "");
+
+}
+});
+
+u21.style.cursor = 'pointer';
+$axure.eventManager.click('u21', function(e) {
+
+if (true) {
+
+	NewTab('http://www.youtube.com/channel/UCNiunGcrtVrMMG2s8JIoNvg/videos/', "");
+
+}
+});
+gv_vAlignTable['u33'] = 'center';
+u34.style.cursor = 'pointer';
+$axure.eventManager.click('u34', function(e) {
+
+if (true) {
+
+	SetPanelState('u14', 'pd2u14','none','',500,'none','',500);
+
+}
+});
