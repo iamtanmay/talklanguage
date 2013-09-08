c084a4a0eadd3ae357bf113deda1b8555706b828
iamtanmay <iamtanmay@gmail.com>
2013-09-08 23:54:26 +0200

    - More edits

--- Main_files/axurerp_pagespecificscript.js
+++ Main_files/axurerp_pagespecificscript.js
@@ -1,4 +1,4 @@
-for(var i = 0; i < 37; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+for(var i = 0; i < 36; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
 
 $axure.eventManager.pageLoad(
 function (e) {
@@ -19,7 +19,7 @@ $axure.eventManager.click('u27', function(e) {
 
 if (true) {
 
-	NewTab('https://sites.google.com/site/iowagapps/google/setting-up-google-hangouts', "");
+	NewTab('http://www.youtube.com/channel/UCNiunGcrtVrMMG2s8JIoNvg/videos/', "");
 
 }
 });
@@ -29,7 +29,7 @@ $axure.eventManager.click('u28', function(e) {
 
 if (true) {
 
-	SetPanelState('u14', 'pd2u14','none','',500,'none','',500);
+	NewTab('http://keepvid.com/', "");
 
 }
 });
@@ -39,7 +39,7 @@ $axure.eventManager.click('u29', function(e) {
 
 if (true) {
 
-	NewTab('https://support.google.com/plus/answer/1355579?hl=en', "");
+	NewTab('http://snipmp3.com/', "");
 
 }
 });
@@ -55,6 +55,16 @@ if (true) {
 
 }
 });
+
+u30.style.cursor = 'pointer';
+$axure.eventManager.click('u30', function(e) {
+
+if (true) {
+
+	NewTab('http://keepvid.com/', "");
+
+}
+});
 document.getElementById('u6_img').tabIndex = 0;
 HookHover('u6', false);
 
@@ -67,19 +77,7 @@ if (true) {
 
 }
 });
-document.getElementById('u35_img').tabIndex = 0;
-HookHover('u35', false);
-
-u35.style.cursor = 'pointer';
-$axure.eventManager.click('u35', function(e) {
-
-if (true) {
-
-	SetPanelState('u14', 'pd3u14','none','',500,'none','',500);
-
-}
-});
-gv_vAlignTable['u13'] = 'center';document.getElementById('u4_img').tabIndex = 0;
+gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u13'] = 'center';document.getElementById('u4_img').tabIndex = 0;
 HookHover('u4', false);
 
 u4.style.cursor = 'pointer';
@@ -91,27 +89,7 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u7'] = 'center';
-u23.style.cursor = 'pointer';
-$axure.eventManager.click('u23', function(e) {
-
-if (true) {
-
-	NewTab('http://snipmp3.com/', "");
-
-}
-});
-
-u24.style.cursor = 'pointer';
-$axure.eventManager.click('u24', function(e) {
-
-if (true) {
-
-	NewTab('http://keepvid.com/', "");
-
-}
-});
-document.getElementById('u2_img').tabIndex = 0;
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u26'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u7'] = 'center';document.getElementById('u2_img').tabIndex = 0;
 HookHover('u2', false);
 
 u2.style.cursor = 'pointer';
@@ -123,7 +101,17 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u18'] = 'center';document.getElementById('u10_img').tabIndex = 0;
+gv_vAlignTable['u18'] = 'center';
+u19.style.cursor = 'pointer';
+$axure.eventManager.click('u19', function(e) {
+
+if (true) {
+
+	NewTab('https://sites.google.com/site/iowagapps/google/setting-up-google-hangouts', "");
+
+}
+});
+document.getElementById('u10_img').tabIndex = 0;
 HookHover('u10', false);
 
 u10.style.cursor = 'pointer';
@@ -135,33 +123,35 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u5'] = 'center';
-u22.style.cursor = 'pointer';
-$axure.eventManager.click('u22', function(e) {
+
+u20.style.cursor = 'pointer';
+$axure.eventManager.click('u20', function(e) {
 
 if (true) {
 
-	NewTab('http://keepvid.com/', "");
+	NewTab('https://support.google.com/plus/answer/1355579?hl=en', "");
 
 }
 });
-
-u21.style.cursor = 'pointer';
-$axure.eventManager.click('u21', function(e) {
+gv_vAlignTable['u22'] = 'center';
+u33.style.cursor = 'pointer';
+$axure.eventManager.click('u33', function(e) {
 
 if (true) {
 
-	NewTab('http://www.youtube.com/channel/UCNiunGcrtVrMMG2s8JIoNvg/videos/', "");
+	SetPanelState('u14', 'pd1u14','none','',500,'none','',500);
 
 }
 });
-gv_vAlignTable['u33'] = 'center';
+document.getElementById('u34_img').tabIndex = 0;
+HookHover('u34', false);
+
 u34.style.cursor = 'pointer';
 $axure.eventManager.click('u34', function(e) {
 
 if (true) {
 
-	SetPanelState('u14', 'pd2u14','none','',500,'none','',500);
+	SetPanelState('u14', 'pd3u14','none','',500,'none','',500);
 
 }
 });
